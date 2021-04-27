3ea38030e01df35d4bf8ffca59f6b1bbad76e6e9
ustbly <ustbly2021@126.com>
2021-04-27 10:12:21 +0800

    hot-fix commit2

--- src/main/java/com/ustb/ly/GitTest.java
+++ src/main/java/com/ustb/ly/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello GitHub!");
         System.out.println("Hello GitLab!");
         System.out.println("Hello Gitee!");
+        System.out.println("Hot-Fix test!");
     }
 }
